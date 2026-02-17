a073af68ed758f3c2b856506658c3d6987be8009
k1ndr0-dev <faysal.bou10@gmail.com>
2026-02-17 17:09:49 +0100

    Update BorrarPantalla.py

--- PRINCIPAL/FUNCIONES/BorrarPantalla.py
+++ PRINCIPAL/FUNCIONES/BorrarPantalla.py
@@ -1,7 +1,25 @@
 import os
+import time
 
 def clean():
 
     os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def clean_espera(segundos: int):
+
+    clean() #Limpiamos para asegurar que se quede limpia la pantalla cuando empiece la cuenta atrás
+
+    for i in range (1, segundos+1):
+
+        print(f"{i}...")
+        time.sleep(1)
+        clean()
+
+
+
+    
+        
+    
 
 
