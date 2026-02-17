14ca7a150860e6793e6d1dffe7b8ec6f26ea92fe
k1ndr0-dev <faysal.bou10@gmail.com>
2026-02-17 17:10:28 +0100

    Update nombres.py

--- PRINCIPAL/FUNCIONES/nombres.py
+++ PRINCIPAL/FUNCIONES/nombres.py
@@ -3,6 +3,8 @@
 #Capacidad de eliminar jugadores y modificar
 #Devuelve una lista de jugadores
 
+from .BorrarPantalla import clean as LimpiarPantalla 
+
 
 
 def instrucciones():
@@ -38,70 +40,91 @@ def funcion_lista_jugadores():
 
             iterador -= 1 #Decrementamos el iterador para no saltarnos el siguiente espacio de la lista
 
-            #PEDIDA DE DATOS
 
-
-            print("¿Qué jugador desea modificar?")
-
-            #Imprimir lista de jugadores
-            for i in range(0,len(jugadores)):
-
-                print(i+1,". "+jugadores[i])
-
+            #COMPROBACIÓN
             
-            
-            while True:
+            if len(jugadores) == 0:
+                input("No hay ningún jugador introducido. Pulse cualquier tecla")
+                LimpiarPantalla()
+            else:
 
-                #El usuario introduce por teclado un string
 
-                eleccion = input()
 
-                #En caso de darle al Intro, cierra el bucle
-                if eleccion == "":
 
-                    break
+                #PEDIDA DE DATOS
+
+
+                print("¿Qué jugador desea modificar?")
+
+                #Imprimir lista de jugadores
+                for i in range(0,len(jugadores)):
+
+                    print(i+1,". "+jugadores[i])
+
                 
+                
+                while True:
 
-                #En el resto de casos
-                else:
+                    #El usuario introduce por teclado un string
 
+                    eleccion = input()
 
-                    try:
+                    #En caso de darle al Intro, cierra el bucle
+                    if eleccion == "":
 
-                        #Intentamos convertir el str introducido a un número entero (int)
-                        eleccion = int(eleccion.strip()) #En caso de error, salta a except ValueError
+                        break
+                    
 
-
-
-                        #Comprobar si el número introducido está dentro del rango de jugadores introducidos
-
-                        if eleccion < 1 or eleccion > len(jugadores):
-
-                            print(f"Debes de introducir un número dentro del rango de jugadores (1-{len(jugadores)})")
+                    #En el resto de casos
+                    else:
 
 
+                        try:
 
-                        else: #En caso de ser un número correcto
-
-                            print("Introduce el nombre del jugador")
-                            jugadores[eleccion-1] = input()
-
-                            eleccion = None #Vaciamos los datos que haya introducido el usuario
-
-                            break #Finalizamos el bucle
+                            #Intentamos convertir el str introducido a un número entero (int)
+                            eleccion = int(eleccion.strip()) #En caso de error, salta a except ValueError
 
 
 
-                    except ValueError: #En caso de ser un dato incorrecto
+                            #Comprobar si el número introducido está dentro del rango de jugadores introducidos
 
-                        print("Tienes que introducir un número entero")
+                            if eleccion < 1 or eleccion > len(jugadores):
+
+                                print(f"Debes de introducir un número dentro del rango de jugadores (1-{len(jugadores)})")
+
+
+
+                            else: #En caso de ser un número correcto
+
+                                print("Introduce el nombre del jugador")
+                                jugadores[eleccion-1] = input()
+
+                                eleccion = None #Vaciamos los datos que haya introducido el usuario
+
+                                break #Finalizamos el bucle
+
+
+
+                        except ValueError: #En caso de ser un dato incorrecto
+
+                            print("Tienes que introducir un número entero")
 
 
             
 
         elif(jugadores[iterador] == ""): 
 
-            jugadores.pop(iterador) #Eliminamos el espacio creado para abandonar el proceso
+            if len(jugadores) == 0:
+                input("La lista está vacía. Pulse cualquier tecla")
+                LimpiarPantalla()
 
-            return jugadores #Finaliza la función y devuelve la lista de jugadores
+            elif len(jugadores) < 3:
+                input("No hay suficientes jugadores metidos. Pulse cualquier tecla")
+                LimpiarPantalla()
+                
+            else:
+
+                jugadores.pop(iterador) #Eliminamos el espacio creado para abandonar el proceso
+
+                return jugadores #Finaliza la función y devuelve la lista de jugadores
 
